e74b60cba641502eef0924a6a8e599a06eca2541
Sellig6792 <gilles.meunier6792@gmail.com>
2021-09-19 16:18:46 +0200

    add setup.py and rename package to apscrypt from aps_crypt

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="apscrypt",
+    packages=["apscrypt"],
+    versions="1.0.0",
+    author="Sellig6792",
+    license="LICENSE",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/Sellig6792/APSCrypt",
+    description="The easy-to-use crypter.",
+    python_requires='>=3.6',
+)
